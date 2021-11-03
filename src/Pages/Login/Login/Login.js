c4ff753465d4cbe385b6b3c9317b249a753509f8
nazmul128 <shafayat536@gmail.com>
2021-11-03 16:23:10 +0600

    page redirect

--- src/Pages/Login/Login/Login.js
+++ src/Pages/Login/Login/Login.js
@@ -4,6 +4,8 @@ import { useState } from "react";
 import initializeAuthentication from '../Firebase/firebase.init'
 import './Login.css'
 import { Button } from 'react-bootstrap';
+import { useHistory } from 'react-router-dom';
+import { useLocation } from 'react-router-dom';
 
 
 initializeAuthentication();
@@ -17,13 +19,19 @@ const Login = () => {
     // const [password, setPassword] = useState('');
     // const [error, setError] = useState('');
     // const [isLogin, setIsLogin] = useState(false);
+    const location=useLocation();
+    const history=useHistory();
+    const redirect_uri=location.state?.from || '/';
 
     const auth = getAuth();
+
+
 
     const handleGoogleSignIn = () => {
         signInWithPopup(auth, googleProvider)
             .then(result => {
                 const user = result.user;
+                history.push(redirect_uri);
                 // console.log(user);
             })
     }
@@ -45,7 +53,6 @@ const Login = () => {
    
     return (
         <div>
-            
             <Button className='google' onClick={handleGoogleSignIn}><i class="fab fa-google"></i>Google Sign In</Button>
         </div>
     );
